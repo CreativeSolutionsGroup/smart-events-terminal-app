1980d12fd66ccffbc6959028dee7dc6d8ada317e
kcouv <kcouvion@cedarville.edu>
2022-08-17 15:37:21 -0400

    Installed websocket and types

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+// Heartbeat pulse work stuff thing
+
+// imported stuff, not sure which ones are really necessary
+let https = require('https');
+let fs = require('fs');
+let WebSocketServer = require('websocket').server;
+
