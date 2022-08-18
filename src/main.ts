d583266c831f4bcc8f95e7eecc6f4cc5f570556a
kcouv <kcouvion@cedarville.edu>
2022-08-18 11:26:40 -0400

    Initialized client and server ts files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-// Heartbeat pulse work stuff thing
+// begin Heartbeat client pulse work stuff things
 
 // imported stuff, not sure which ones are really necessary
-let https = require('https');
-let fs = require('fs');
-let WebSocketServer = require('websocket').server;
+import https from 'https';
+import fs from 'fs';
+import WebSocketServer from 'websocket';
 
+// establish connection with backend
+let url: string;
+let protocol: string;
+let webSocket = new WebSocket(url, protocol);
+
+// end heartbeat stuff things
---
+++ src/test-backend.ts
@@ -0,0 +1,8 @@
+//#!/usr/bin/env node
+// previous line is shebang but idk how it works
+
+// test server to establish websocket connection
+
+import https from 'https';
+import fs from 'fs';
+import WebSocketServer from 'websocket';
