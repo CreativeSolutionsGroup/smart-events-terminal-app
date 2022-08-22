e5aaf9e2f0bf9d21c1f8835d767522f22800efda
kcouv <kcouvion@cedarville.edu>
2022-08-22 15:00:28 -0400

    Updated from pull request comments

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-// begin Heartbeat client pulse work stuff things
-
-// imports, as opposed to exports
-import https from 'https';
-import fs from 'fs';
-import WebSocketServer from 'websocket';
 import ws from 'ws';
-import { time } from 'console';
 
-// relevant functions
-function connect() {
-    let serverUrl: string = "wss://localhost:3001"; // correct server url??
-    let scheme: string = "wss";
-}
-// establish connection with backend
-let url: string;
-let protocol: string;
-//let webSocket = new WebSocket(url, protocol);
-
-// ws test
+// Build client node
 let client = new ws('ws://localhost:3001/heartbeat');
+
+// Define functions for client node
 client.on('open', () => {
     setInterval(() => {
         client.send('RPI 1: Still breathing. Runtime: ' + Date.now());
@@ -28,6 +13,3 @@ client.on('open', () => {
 client.on('close', () => {
     client.send('Dead');
 });
-
-
-// end heartbeat stuff things
