a0582331bcca25d985156190b690d3f096e9ce0e
SebastianNeiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2022-09-02 18:23:32 -0400

    WS does not crash program
    Errors send a log, and if it connects and disconnects the program is still good. Test starting the terminal, then the backend, wait for the heartbeat to go through, then close the backend to make sure the program still is able to survive.
    
    Potentially if you close the backend at the exact millisecond that the if statement verifies the WS is still open, then you could still crash the program when it tries to send the message.

--- src/main.ts
+++ src/main.ts
@@ -11,31 +11,42 @@ dotenv.config()
 axios.defaults.baseURL = process.env.BACKEND_URL ?? "http://localhost:3001/v1";
 const heartbeat_url =  process.env.HEARTBEAT_URL ?? "ws://localhost:3001";
 
-// Build client node
-let client = new ws(`${heartbeat_url}/heartbeat`);
-
-//catch WS errors
-client.on('error', (error) => {
-  console.log('WS error: ', error)
-})
-
-// Define functions for client node
-client.on('open', () => {
-  client.send(JSON.stringify({
-    mac_address: getmac()
-  } as Heartbeat));
+const buildHeartbeat = async (backoff: number) => {
+  await new Promise(f => setTimeout(f, backoff * 1000));
+  // Build client node
+  let client = new ws(`${heartbeat_url}/heartbeat`);
+  //Bool is necessary to prevent sending after interval
+  let clientOn = true
   
-  setInterval(() => {
+  //catch WS errors. This is a required function so that the app does not crash
+  client.on('error', (error) => {
+    backoff *= 2
+    if (backoff > 128) { backoff = 128 } else if (backoff === 0) { backoff = 1}
+    console.log('WS error. Attempting Reconnect in ' , backoff , ' seconds')
+    clientOn = false
+  })
+  
+  // Define functions for client node
+  client.on('open', () => {
     client.send(JSON.stringify({
       mac_address: getmac()
     } as Heartbeat));
-  }, 10000)
-});
-
-client.on('close', () => {
-  console.log("closed");
-});
-
+    
+    //This function will still activate after the client closes so the Bool checks that the client is open still
+    setInterval(() => {
+      if (clientOn) {
+        client.send(JSON.stringify({
+          mac_address: getmac()
+        } as Heartbeat));
+      }
+    }, 10000)
+    backoff = 0
+  });
+  
+  client.on('close', () => {
+    buildHeartbeat(backoff)
+  });
+}
 
 let rl = readline.createInterface({
   input: process.stdin,
@@ -64,4 +75,5 @@ const sendCheckIn = async (checkin: Checkin, backoff: number) => {
   }
 }
 
+buildHeartbeat(0)
 waitForInput()
