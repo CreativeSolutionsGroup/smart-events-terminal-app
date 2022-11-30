dab3fe97f6dbb2635e5bf48175f22a5fe8bbf4bd
spbills <spencebills0@gmail.com>
2022-11-30 16:01:44 -0500

    Added more error handling

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,14 @@ import { Checkin } from './models/Checkin';
 import { initialize_database } from './services/orm';
 import { delete_check_in, insert_check_in } from './services/checkin';
 
+const DEFAULT_MAC = "00:00:00:00:00:0A";
+
 const get_mac = (): string => {
   try {
     return getmac()
   } catch (error) {
-    console.log("Not connected to an interface :(");
-    return "00:00:00:00:00:00"
+    console.error("Not connected to an interface :(");
+    return DEFAULT_MAC
   }
 }
 
@@ -29,8 +31,8 @@ const heartbeat_url = process.env.HEARTBEAT_URL ?? "ws://localhost:3001";
 
 const build_heartbeat = async (backoff: number) => {
   await new Promise(f => setTimeout(f, backoff * 1000));
-  // Build client node
   let client = new ws(`${heartbeat_url}/heartbeat`);
+
   //Bool is necessary to prevent sending after interval
   let clientOn = true
 
@@ -71,9 +73,15 @@ let rl = readline.createInterface({
 
 const wait_for_input = () => {
   rl.question('Input ID:\n', async (idNum) => {
+    // this is necessary so we can take in more than one input at a time.
+    // instantly starts the next "question"
     wait_for_input()
+
     const check_in = new Checkin();
     check_in.mac_address = get_mac()
+
+    // the normal case- if the id number is a correct length, do normal checkin.
+    // otherwise, if it's longer, then go back ID_LENGTH (get the appended ID to a perhaps garbage string)
     if (idNum.length === ID_LENGTH) {
       if (!isNaN(Number(idNum))) {
         check_in.student_id = idNum
@@ -90,10 +98,15 @@ const wait_for_input = () => {
 }
 
 const read_cache = async () => {
-  const cache = await Checkin.find()
-  await Promise.all(cache.map(async (checkin) => {
-    await send_check_in(checkin)
-  }))
+  try {
+    const cache = await Checkin.find()
+    await Promise.all(cache.map(async (checkin) => {
+      await send_check_in(checkin)
+    }));
+  } catch (e) {
+    console.error("Failed to read cache. Got the following error:")
+    console.error(e);
+  }
 }
 
 const cache_observer = () => {
